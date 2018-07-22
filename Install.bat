49969f7d2052faa0fccac5108cb1ed61d97f0058
Zplutor <zplutor@qq.com>
2018-07-22 20:10:04 +0800

    Initial commit.

---
+++ Install.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set exe_path="%~dp0Release\CopyPath.exe"
+
+if not exist %exe_path% (
+	echo %exe_path% not found. 
+	echo Please build the solution with release configuration, 
+	echo   or modify this script to point to an existent executable file.
+	echo Install failed.
+	goto :end
+)
+
+set reg_path=HKEY_CURRENT_USER\Software\Classes
+
+reg add %reg_path%\*\shell\CopyPath /f /ve /d "Copy path"
+reg add %reg_path%\*\shell\CopyPath\command /f /ve /d "%exe_path% -p %%1"
+reg add %reg_path%\*\shell\CopyName /f /ve /d "Copy name"
+reg add %reg_path%\*\shell\CopyName\command /f /ve /d "%exe_path% -n %%1"
+
+reg add %reg_path%\Folder\shell\CopyPath /f /ve /d "Copy path"
+reg add %reg_path%\Folder\shell\CopyPath\command /f /ve /d "%exe_path% -p %%1"
+reg add %reg_path%\Folder\shell\CopyName /f /ve /d "Copy name"
+reg add %reg_path%\Folder\shell\CopyName\command /f /ve /d "%exe_path% -n %%1"
+
+echo Install succeeded.
+
+:end
+pause
---
+++ main.c
@@ -0,0 +1,87 @@
+#include <Windows.h>
+#include <Shlwapi.h>
+#include <strsafe.h>
+
+static DWORD SetStringToClipboard(LPCWSTR string);
+static DWORD CopyStringToMemory(LPCWSTR string, int copied_length, HGLOBAL memory_handle);
+
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+    int argument_count = 0;
+    LPWSTR* arguments = CommandLineToArgvW(GetCommandLine(), &argument_count);
+    if (argument_count < 3) {
+        return ERROR_INVALID_PARAMETER;
+    }
+
+    BOOL name_only = FALSE;
+    if (lstrcmp(arguments[1], L"-n") == 0) {
+        name_only = TRUE;
+    }
+    else if (lstrcmp(arguments[1], L"-p") == 0) {
+        name_only = FALSE;
+    }
+    else {
+        return ERROR_INVALID_PARAMETER;
+    }
+
+    LPWSTR string_to_set = arguments[2];
+    if (name_only) {
+        string_to_set = PathFindFileName(string_to_set);
+    }
+
+    BOOL is_succeeded = OpenClipboard(NULL);
+    if (!is_succeeded) {
+        return GetLastError();
+    }
+
+    DWORD error = SetStringToClipboard(string_to_set);
+    CloseClipboard();
+
+    return error;
+}
+
+
+static DWORD SetStringToClipboard(LPCWSTR string) {
+
+    int string_length = lstrlen(string);
+    int memory_size = (string_length + 1) * sizeof(wchar_t);
+
+    HGLOBAL memory_handle = GlobalAlloc(GMEM_MOVEABLE, memory_size);
+    if (memory_handle == NULL) {
+        return GetLastError();
+    }    
+
+    DWORD error = CopyStringToMemory(string, string_length, memory_handle);
+    if (error != ERROR_SUCCESS) {
+        GlobalFree(memory_handle);
+        return error;
+    }
+
+    HANDLE result = SetClipboardData(CF_UNICODETEXT, memory_handle);
+    error = GetLastError();
+    if (result == NULL) {
+        GlobalFree(memory_handle);
+        return error;
+    }
+
+    return ERROR_SUCCESS;
+}
+
+
+static DWORD CopyStringToMemory(LPCWSTR string, int copied_length, HGLOBAL memory_handle) {
+
+    LPVOID memory = GlobalLock(memory_handle);
+    if (memory == NULL) {
+        return GetLastError();
+    }
+
+    HRESULT result = StringCchCopy(memory, copied_length + 1, string);
+    GlobalUnlock(memory_handle);
+
+    if (FAILED(result)) {
+        return ERROR_OUTOFMEMORY;
+    }
+
+    return ERROR_SUCCESS;
+}
