d1cd6d5f15a2d6269d6c414a5ed36daa7ee10978
Zplutor <zplutor@qq.com>
2018-08-28 22:49:31 +0800

    Add double quote to arguments.

--- Install.bat
+++ Install.bat
@@ -13,14 +13,14 @@ if not exist %exe_path% (
 set reg_path=HKEY_CURRENT_USER\Software\Classes
 
 reg add %reg_path%\*\shell\CopyPath /f /ve /d "Copy path"
-reg add %reg_path%\*\shell\CopyPath\command /f /ve /d "%exe_path% -p %%1"
+reg add %reg_path%\*\shell\CopyPath\command /f /ve /d "%exe_path% -p \"%%1\""
 reg add %reg_path%\*\shell\CopyName /f /ve /d "Copy name"
-reg add %reg_path%\*\shell\CopyName\command /f /ve /d "%exe_path% -n %%1"
+reg add %reg_path%\*\shell\CopyName\command /f /ve /d "%exe_path% -n \"%%1\""
 
 reg add %reg_path%\Folder\shell\CopyPath /f /ve /d "Copy path"
-reg add %reg_path%\Folder\shell\CopyPath\command /f /ve /d "%exe_path% -p %%1"
+reg add %reg_path%\Folder\shell\CopyPath\command /f /ve /d "%exe_path% -p \"%%1\""
 reg add %reg_path%\Folder\shell\CopyName /f /ve /d "Copy name"
-reg add %reg_path%\Folder\shell\CopyName\command /f /ve /d "%exe_path% -n %%1"
+reg add %reg_path%\Folder\shell\CopyName\command /f /ve /d "%exe_path% -n \"%%1\""
 
 echo Install succeeded.
 
